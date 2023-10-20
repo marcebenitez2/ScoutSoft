ea1a5a8b0fcd02e66f49ca7fd9c6ef8aaea541b4
marcebenitez2 <marcebenitez0607@gmail.com>
2023-10-20 16:21:06 -0300

    Inicio inventario

--- src/pages/secciones/inventario.jsx
+++ src/pages/secciones/inventario.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import Navbar from "../../components/navbar";
+import { useState } from "react";
+import { useEffect } from "react";
+import { fetchBD } from "../../services/fetchBD";
 
 function Inventario() {
+  const [inventario, setInventario] = useState([]);
+  const [modalOpen, setmodalOpen] = useState(false);
+  const [seleccionada, setSeleccionada] = useState(null);
+
+  useEffect(() => {
+    fetchBD(setInventario, "http://localhost/inventario.php")
+  }, [])
+
   return (
     <main>
       <div className="w-screen min-h-screen flex flex-col pt-4 pb-6 px-16 gap-4 mdn:px-0 mdn:pt-0 overflow-x-hidden dark:bg-custon-black ">
