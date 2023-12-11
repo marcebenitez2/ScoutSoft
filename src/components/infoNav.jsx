d64210a2cb54e66be5844b07d58a0879d81f2dd2
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-11 10:50:50 -0300

    Refactor navigation links and add scoutismo section

--- src/components/infoNav.jsx
+++ src/components/infoNav.jsx
@@ -7,21 +7,11 @@ function InfoNav() {
   return (
     <nav className="w-full h-12 flex fixed justify-between px-10 items-center bg-red-600 text-white font-semibold smn:px-1 z-50">
       <div>
-        <Link to={'/'}>
+        <Link to={"/"}>
           <ImHome size={30} />
         </Link>
       </div>
-      <div className="flex gap-11 smn:gap-3">
-        <Link to={'/nosotros'}>
-          <span>Nosotros</span>
-        </Link>
-        <Link to={'/scoutismo'}>
-          <span>Scoutismo</span>
-        </Link>
-        <Link to={'/formulario'}>
-          <span>Formulario</span>
-        </Link>
-      </div>
+      <div className="flex gap-11 smn:gap-3"></div>
       <Link to={"/login"}>
         <FaUser size={30} />
       </Link>
